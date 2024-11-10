2f71062335707de9b3c1fcef6575139b3a1bc78b
meronpanun <fko2447021@stu.o-hara.ac.jp>
2024-11-10 21:00:37 +0900

    Player
    2D vector class and player

--- JUMPMANADVENTURE/JUMPMANADVENTURE/Player.h
+++ JUMPMANADVENTURE/JUMPMANADVENTURE/Player.h
@@ -1,5 +1,30 @@
 #pragma once
+#include "Vec2.h"
+
+/// <summary>
+/// プレイヤークラス
+/// </summary>
 class Player
 {
+public:
+	Player();
+	~Player();
+
+	void Init();
+	void Update();
+	void Draw();
+
+	// プレイヤーの上下左右情報取得
+	float GetLeft() const;
+	float GetRight() const;
+	float GetTop() const;
+	float GetBotton() const;
+
+	int GetHp() const { return m_hp; }
+
+
+private:
+	// プレイヤーのHP
+	int m_hp;
 };
 
