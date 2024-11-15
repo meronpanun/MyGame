8b633f7ae54561681091ecd1e88ee430ae9c1db6
meronpanun <fko2447021@stu.o-hara.ac.jp>
2024-11-15 12:42:09 +0900

    up

--- JUMPMANADVENTURE/JUMPMANADVENTURE/Rect.cpp
+++ JUMPMANADVENTURE/JUMPMANADVENTURE/Rect.cpp
@@ -1,1 +1,19 @@
 #include "Rect.h"
+
+Rect::Rect()
+{
+}
+
+Vec2 Rect::GetCenter()
+{
+    return Vec2();
+}
+
+bool Rect::isCol(const Rect& rect)
+{
+    return false;
+}
+
+void Rect::set_TL_Size(Vec2 posTL, Vec2 size)
+{
+}
--- JUMPMANADVENTURE/JUMPMANADVENTURE/Rect.h
+++ JUMPMANADVENTURE/JUMPMANADVENTURE/Rect.h
@@ -7,16 +7,28 @@
 class Rect
 {
 public:
+	Rect();
+	float GetWidth() const { return right - left; }
+	float GetHeigth() const { return bottom - top; }
+
+	// 各座標取得
+	Vec2 GetCenter();
+
+	// 当たり判定
+	bool isCol(const Rect& rect);
+	/// <summary>
+	/// 左上座標とサイズからRectデータを設定
+	/// </summary>
+	/// <param name="posTL">左上座標</param>
+	/// <param name="size">サイズ</param>
+	void set_TL_Size(Vec2 posTL, Vec2 size);
+public:
 	float top;
 	float bottom;
 	float left;
 	float right;
-//public:
-//	Rect() :
-//		top(0),
-//		bottom(0),
-//		left(0),
-//		right(0)
+
+
 
 };
 
